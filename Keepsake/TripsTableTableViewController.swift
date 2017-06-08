19ed3b0cc16b851267956ff5e48df8186203b297
Dominic Pilla <dominicpilla@dominics-mbp>
2017-06-08 17:13:46 -0500

    TripsTable Dequeue

--- Keepsake/TripsTableTableViewController.swift
+++ Keepsake/TripsTableTableViewController.swift
@@ -37,15 +37,15 @@ class TripsTableTableViewController: UITableViewController {
         return 1
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "trip", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
