528fc7d695e5e95a00c785549d5a9c0e626b3752
Dominic Pilla <dominicpilla@dominics-mbp>
2017-06-08 16:07:32 -0500

    Updated My Trip Table

---
+++ Keepsake/TripTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TripTableViewCell.swift
+//  Keepsake
+//
+//  Created by Dominic Pilla on 6/8/17.
+//  Copyright © 2017 Dominic Pilla. All rights reserved.
+//
+
+import UIKit
+
+class TripTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
